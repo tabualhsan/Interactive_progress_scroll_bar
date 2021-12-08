5e65a5be1697087a6ade75ae0ff4d9bb48d79fbe
Tamara Abualhsan <tabualhsan@gmail.com>
2021-12-08 10:40:54 -0800

    initilized

---
+++ app.js
@@ -0,0 +1,15 @@
+const progressBar = document.querySelector('#progress-bar');
+const section =document.querySelector('section');
+
+const animateProgressBar = () => {
+    // gives the distance when your scroll in pixels 
+    let scrollDistance = -section.getBoundingClientRect().top;
+    let progressWidth = (scrollDistance / (section.getBoundingClientRect().height - document.documentElement.clientHeight)) * 100
+    console.log(scrollDistance);
+
+    let value = Math.floor(progressWidth);
+    progressBar.style.width = value + "%";
+    
+};
+
+window.addEventListener("scroll", animateProgressBar);
